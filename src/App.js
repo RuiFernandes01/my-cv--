d905eab39b41ad0c46054c5545a7aac996219980
RuiFernandes01 <63199206+RuiFernandes01@users.noreply.github.com>
2023-10-20 16:24:38 +0100

    react router change

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
-import React from 'react';
-import Navbar from './components/navbar';
-import Home from './components/Home';
-import AboutMe from './components/AboutMe';
-import WorkExperience from './components/WorkExperience';
-import Education from './components/Education';
-import Skills from './components/Skills';
-import{ BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import './App.css';
-
+import React from "react";
+import Navbar from "./components/navbar";
+import Home from "./components/Home";
+import AboutMe from "./components/AboutMe";
+import WorkExperience from "./components/WorkExperience";
+import Education from "./components/Education";
+import Skills from "./components/Skills";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
-    <Router>
+    <Router basename="ruifernandes01.cv.io">
       <Navbar />
       <Routes>
-  <Route path="/" element={<Home />} />
-  <Route path="/aboutme" element={<AboutMe />} />
-  <Route path="/workexperience" element={<WorkExperience />} />
-  <Route path="/education" element={<Education />} />
-  <Route path="/skills" element={<Skills />} />
-</Routes>
-
-
+        <Route path="/" element={<Home />} />
+        <Route path="/aboutme" element={<AboutMe />} />
+        <Route path="/workexperience" element={<WorkExperience />} />
+        <Route path="/education" element={<Education />} />
+        <Route path="/skills" element={<Skills />} />
+      </Routes>
     </Router>
   );
 }
